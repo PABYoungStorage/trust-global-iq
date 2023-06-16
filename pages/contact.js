21d8143ea2be882fc17f0cb374792652d3e8a6fc
hari13172 <haridiablo72@gmail.com>
2023-06-16 19:43:23 +0000

    updated contact and about page

--- pages/contact.js
+++ pages/contact.js
@@ -9,28 +9,35 @@ const Info = () => {
   return (
     <div className="imagecontainer">
       <div className="into">
+        <img />
         <h1>Contact Us</h1>
       </div>
-    <div className="dent">
-      <div className="details">
-        <h1>Send Us a Message</h1>
-        <p>Your email address will not be published. Required fields are marked </p>
-        <div className="box">
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Email" />
-        <input type="number" placeholder="phNo" />
-        <input type="text" placeholder="subject" />
-        <input type="text" placeholder="message" />
-        </div>
+      <div className="dent">
+        <div className="details">
+          <h1>Send Us a Message</h1>
+          <p>Your email address will not be published. Required fields are marked </p>
+          <div className="box">
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Email" />
+            <input type="number" placeholder="phNo" />
+            <input type="text" placeholder="subject" />
+            <input type="text" placeholder="message" />
+          </div>
 
+        </div>
+        <div className="demo">
+          <h2>Contact Address</h2>
+          <span>167. V.O.C Nagar, K.N.Colony Post, Salem- 636014</span>
+          <img alt="" src="/Icons/email.png" className="msg"/>
+          <span>iqglobaltrust@gmail.com</span>
+          <img alt="" src="/Icons/email.png" className="msg1"/>
+          <span>+917904060790</span>
+          <img alt="" src="/Icons/email.png" className="msg2"/>
+        </div>
       </div>
-      <div className="demo">
-        <h2>Contact Address</h2>
-       <span>167. V.O.C Nagar, K.N.Colony Post, Salem- 636014</span>
-       <span>iqglobaltrust@gmail.com</span>
-       <span>+917904060790</span>
-      </div>
-    </div>
+        <div className="button">
+          <button className="btn">Submit</button>
+        </div>
     </div>
 
   );
